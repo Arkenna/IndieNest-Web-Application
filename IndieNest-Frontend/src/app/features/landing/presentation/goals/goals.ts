4a59029c72bf63474e14392f8bc6631fb95c9d5d
SimpleGP <giulianopv2@gmail.com>
2025-10-09 10:22:11 -0500

    Elimina carpeta IndieNest-Frontend del repositorio

---
+++ IndieNest-Frontend/src/app/features/landing/presentation/goals/goals.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-goals',
+  templateUrl: './goals.html',
+  styleUrls: ['./goals.css']
+})
+export class GoalsComponent {
+
+}
