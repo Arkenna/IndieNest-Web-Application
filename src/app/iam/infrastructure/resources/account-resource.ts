a966f268106f003f779a7033b55d6d8fde47dafa
Daniel Aquino <u202217678@upc.edu.pe>
2025-10-21 16:25:24 -0500

    feat(iam): add responses
    -add users response
    -add accounts response
    -add base extends for resouses and responses

--- src/app/iam/infrastructure/resources/account-resource.ts
+++ src/app/iam/infrastructure/resources/account-resource.ts
@@ -1,7 +1,8 @@
 import {UserResource} from './user-resource';
 import {AccountType} from '../../domain/model/account-type';
+import {BaseResource} from '../../../shared/infrastructure/base-resource';
 
-export interface AccountResource {
+export interface AccountResource extends BaseResource {
   id: number;
   user: UserResource;
   email: string;
