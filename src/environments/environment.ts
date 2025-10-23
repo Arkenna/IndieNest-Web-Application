f3d09a51b5dbce6ebdcc30615c095a84c904e0ae
Daniel Aquino <u202217678@upc.edu.pe>
2025-10-23 08:27:45 -0500

    feat(profile): add profile context enpoints
    -add profile enpoint path in environments
    -add portfolio endpoint path in environmnets
    -add group projects endpoint path in environments
    -add profiles api endpoint
    -add portfolios api endpoint
    -add group projects api endpoint

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,17 @@
 export const environment = {
-  production: true,
+  production: false,
   apiBaseUrl: "http://localhost:3000/api/v1",
+
   usersEndpointPath: "/users",
   accountsEndpointPath: "/accounts",
+
   reviewsEndpointPath: "/reviews",
+
   artsEndpointPath: "/arts",
   gamesEndpointPath: "/games",
   audiosEndpointPath: "/audios",
+
+  profilesEndpointPath: "/profiles",
+  portfoliosEndpointPath: "/portfolios",
+  groupProjectsEndpointPath: "/group-projects",
 };
