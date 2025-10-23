89cd9cf64ffb983f827cc2a11858a4d0505b46b5
Daniel Aquino <u202217678@upc.edu.pe>
2025-10-23 15:34:41 -0500

    fix(project): fix resources

--- src/app/project/infrastructure/resources/audio-resource.ts
+++ src/app/project/infrastructure/resources/audio-resource.ts
@@ -2,8 +2,13 @@ import {AudioCategory} from '../../domain/model/audio-category';
 import {BaseResource} from '../../../shared/infrastructure/base-resource';
 
 export interface AudioResource extends BaseResource{
-  id:number;
-  audioUrl:string;
-  format:string;
-  category:AudioCategory;
+  id:number,
+  authorId: number,
+  name:string,
+  description:string,
+  rating:number,
+  creationDate:Date,
+  audioUrl:string,
+  format:string,
+  category: AudioCategory
 }
