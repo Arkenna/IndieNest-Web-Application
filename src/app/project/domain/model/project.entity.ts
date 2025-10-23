4784f36371bff02be61c7a8db9504e3441c65d62
Daniel Aquino <u202217678@upc.edu.pe>
2025-10-23 15:10:05 -0500

    fix(project): fix project abstract entity

--- src/app/project/domain/model/project.entity.ts
+++ src/app/project/domain/model/project.entity.ts
@@ -1,43 +1,47 @@
 import {Review} from '../../../support/domain/model/review.entity'
+import {BaseEntity} from '../../../shared/domain/model/base-entity';
 
-export class Project{
-  constructor(project:{id:number, name:string, creationDate:string,author:string,description:string,rating:number, reviews:Review[]}) {
+export class Project implements BaseEntity{
+  constructor(project:{
+    id:number,
+    authorId: number,
+    name:string,
+    description:string,
+    creationDate:Date,
+    rating:number
+  }) {
+
     this._id = project.id;
+    this._authorId = project.authorId;
     this._name = project.name;
     this._description = project.description;
-    this._rating = project.rating;
     this._creationDate = project.creationDate;
-    this._author = project.author;
-    this.reviews = project.reviews;
+    this._rating = project.rating;
   }
-  private _id:number;
+  private readonly _id:number;
+  private readonly _authorId: number; //author's user id
   private _name:string;
-  private _creationDate:string;
-  private _author:string;
   private _description:string;
+  private readonly _creationDate:Date;
   private _rating:number;
-  private reviews:Review[];
 
   get id():number{
     return this._id;
   }
+  get authorId():number{
+    return this._authorId;
+  }
   get name():string{
     return this._name;
-  }
-  get creationDate():string{
-    return this._creationDate;
-  }
-  get author():string{
-    return this._author;
   }
   get description():string{
     return this._description;
   }
+  get creationDate():Date{
+    return this._creationDate;
+  }
   get rating():number{
     return this._rating;
-  }
-  get review():Review[]{
-    return this.reviews;
   }
 
   set name(name:string){
@@ -46,7 +50,6 @@ export class Project{
   set description(description:string){
     this._description = description;
   }
-
   set rating(rating:number){
     this._rating = rating;
   }
