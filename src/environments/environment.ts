a297c713ea01369135ade7d07566c5039e394578
JohnnyGZ41 <johnnyundertale41@outlook.com>
2025-11-15 01:01:17 -0500

    chore() api route

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiBaseUrl: "http://localhost:3000/api/v1",
+  apiBaseUrl: "https://indienest-web-application.onrender.com/api/v1",
 
   usersEndpointPath: "/users",
   accountsEndpointPath: "/accounts",
